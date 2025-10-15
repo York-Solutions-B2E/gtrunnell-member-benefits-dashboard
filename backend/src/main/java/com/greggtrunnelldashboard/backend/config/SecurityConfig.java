56b5335ebaa75c28d0b1e4040736c47a51f0b4a0
Gregg Trunnell <gggtrunnell@gmail.com>
2025-10-15 10:40:26 -0500

    up to test controller for auth. project runs

---
+++ backend/src/main/java/com/greggtrunnelldashboard/backend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.greggtrunnelldashboard.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+
+        return http.build();
+    }
+}
+
--- backend/src/main/java/com/greggtrunnelldashboard/backend/controllers/HelloController.java
+++ backend/src/main/java/com/greggtrunnelldashboard/backend/controllers/HelloController.java
@@ -1,15 +1,27 @@
 package com.greggtrunnelldashboard.backend.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello World!";
+    @GetMapping("/api/public/hello")
+    public String publicHello() {
+
+        return "Hello Public!";
     }
-
-
+    @GetMapping("/api/private/hello")
+    public Map<String, Object> privateHello(@AuthenticationPrincipal Jwt jwt) {
+        return Map.of(
+                "message", "Hello from PRIVATE endpoint!",
+                "email", jwt.getClaimAsString("email"),
+                "sub", jwt.getClaimAsString("sub"),
+                "issuer", jwt.getIssuer().toString()
+        );
+    }
 }
