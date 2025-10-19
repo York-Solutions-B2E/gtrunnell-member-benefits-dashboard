052e6cd2f0888b4b1dd024599f9b03ad03b1820b
Gregg Trunnell <gggtrunnell@gmail.com>
2025-10-18 21:03:06 -0500

    created plans entitiy

---
+++ backend/src/main/java/com/greggtrunnelldashboard/backend/repositories/PlanRepository.java
@@ -0,0 +1,10 @@
+package com.greggtrunnelldashboard.backend.repositories;
+
+import com.greggtrunnelldashboard.backend.entities.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PlanRepository extends JpaRepository<Plan, UUID> {
+
+}
